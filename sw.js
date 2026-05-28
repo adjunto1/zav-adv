3a305a37ce9414bb0b7d931ff4ace12e0c1f920b
adjunto1 <adjunto183@gmail.com>
2026-05-27 21:39:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,19 @@
-const CACHE_NAME = 'zav-v2';
-const FILES = [
-  '/zav-adv/',
-  '/zav-adv/index.html',
-  '/zav-adv/manifest.json',
-  '/zav-adv/icons/icon-192.png',
-  '/zav-adv/icons/icon-512.png'
-];
+const CACHE_NAME = 'zav-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES).catch(()=>{})));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  // Sem cache - sempre busca da rede
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
 });
